6626f36f1e984d47a1609c854ec91301c0d1ef3d
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2020-09-19 18:00:02 +0530

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,13 @@
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
