545b912ad8ea5db85c9a2324dfceac7720746206
Makarand Nikam <64835912+mack-1999@users.noreply.github.com>
2020-09-20 11:32:41 +0530

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -11,3 +11,17 @@ function linkAction(){
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const postLink = document.querySelectorAll('.post_link');   
+
+function linkAction2(){
+  /*Active link*/
+  postLink.forEach(n => n.classList.remove('active2'));
+  this.classList.add('active2');
+  
+  /*Remove menu mobile*/
+  const postMenu = document.getElementById('post-menu')
+  postMenu.classList.remove('show')
+}
+postLink.forEach(n => n.addEventListener('click', linkAction2));
